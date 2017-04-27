96c3d12aaec97ee360ccc0366488c6d8ddde70cf
Jens Lindhardt <jens@lindhardt-freelance.dk>
2017-04-27 23:27:19 +0200

    module 04 and module 05 starter and solution done

---
+++ module 05 - Developing ASP.NET MVC Views/Lab/Solution/PhotoSharingApplication/PhotoSharingApplication/Controllers/PhotoController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Collections.Generic;
+using System.Globalization;
+using PhotoSharingApplication.Models;
+
+namespace PhotoSharingApplication.Controllers
+{
+    [ValueReporter]
+    public class PhotoController : Controller
+    {
+        private PhotoSharingContext context = new PhotoSharingContext();
+
+        //
+        // GET: /Photo/
+
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+        [ChildActionOnly]
+        public ActionResult _PhotoGallery(int number = 0)
+        {
+            List<Photo> photos;
+
+            if (number == 0)
+            {
+                photos = context.Photos.ToList();
+            }
+            else
+            {
+                photos = (from p in context.Photos
+                          orderby p.CreatedDate descending
+                          select p).Take(number).ToList();
+            }
+
+            return PartialView("_PhotoGallery", photos);
+        }
+
+        public ActionResult Display(int id)
+        {
+            Photo photo = context.Photos.Find(id);
+            if (photo == null)
+            {
+                return HttpNotFound();
+            }
+            return View("Display", photo);
+        }
+
+        public ActionResult Create()
+        {
+            Photo newPhoto = new Photo();
+            newPhoto.CreatedDate = DateTime.Today;
+            return View("Create", newPhoto);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Photo photo, HttpPostedFileBase image)
+        {
+            photo.CreatedDate = DateTime.Today;
+            if (!ModelState.IsValid)
+            {
+                return View("Create", photo);
+            }
+            else
+            {
+                if (image != null)
+                {
+                    photo.ImageMimeType = image.ContentType;
+                    photo.PhotoFile = new byte[image.ContentLength];
+                    image.InputStream.Read(photo.PhotoFile, 0, image.ContentLength);
+                }
+                context.Photos.Add(photo);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Photo photo = context.Photos.Find(id);
+            if (photo == null)
+            {
+                return HttpNotFound();
+            }
+            return View("Delete", photo);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Photo photo = context.Photos.Find(id);
+            context.Photos.Remove(photo);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public FileContentResult GetImage(int id)
+        {
+            Photo photo = context.Photos.Find(id);
+            if (photo != null)
+            {
+                return File(photo.PhotoFile, photo.ImageMimeType);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
