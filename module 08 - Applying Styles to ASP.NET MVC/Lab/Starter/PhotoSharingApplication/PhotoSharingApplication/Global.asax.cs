ded6af048890eb28fa95d4685ba43a89b01aa269
Jens Lindhardt <jens@lindhardt-freelance.dk>
2017-05-03 22:02:40 +0200

    Module 08 startup

---
+++ module 08 - Applying Styles to ASP.NET MVC/Lab/Starter/PhotoSharingApplication/PhotoSharingApplication/Global.asax.cs
@@ -0,0 +1,25 @@
+﻿using PhotoSharingApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace PhotoSharingApplication
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            //Seed the database with sample data for development. This code should be removed for production.
+            Database.SetInitializer<PhotoSharingContext>(new PhotoSharingInitializer());
+
+            AreaRegistration.RegisterAllAreas();
+
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
---
+++ module 08 - Applying Styles to ASP.NET MVC/Lab/Starter/PhotoSharingApplication/PhotoSharingTest/RoutingTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PhotoSharingTest.Doubles;
+using System.Web.Routing;
+using PhotoSharingApplication;
+using System.Web.Mvc;
+
+namespace PhotoSharingTest
+{
+    [TestClass]
+    public class RoutingTests
+    {
+
+        [TestMethod]
+        public void Test_Default_Route_ControllerOnly()
+        {
+            //This test checks the default route when only the controller is specified
+            //Arrange
+            var context = new FakeHttpContextForRouting(requestUrl: "~/ControllerName");
+            var routes = new RouteCollection();
+            PhotoSharingApplication.RouteConfig.RegisterRoutes(routes);
+
+            // Act
+            RouteData routeData = routes.GetRouteData(context);
+
+            // Assert
+            Assert.IsNotNull(routeData);
+            Assert.AreEqual("ControllerName", routeData.Values["controller"]);
+            Assert.AreEqual("Index", routeData.Values["action"]);
+            Assert.AreEqual(UrlParameter.Optional, routeData.Values["id"]);
+        }
+
+        [TestMethod]
+        public void Test_PhotoRoute_With_PhotoID()
+        {
+            //This test checks the PhotoRoute route. 
+            //Arrange
+            var context = new FakeHttpContextForRouting(requestUrl: "~/photo/2");
+            var routes = new RouteCollection();
+            PhotoSharingApplication.RouteConfig.RegisterRoutes(routes);
+
+            // Act
+            RouteData routeData = routes.GetRouteData(context);
+
+            // Assert
+            Assert.IsNotNull(routeData);
+            Assert.AreEqual("Photo", routeData.Values["controller"]);
+            Assert.AreEqual("Display", routeData.Values["action"]);
+            Assert.AreEqual("2", routeData.Values["id"]);
+        }
+
+        [TestMethod]
+        public void Test_PhotoTitleRoute_WithTitle()
+        {
+            //This test checks the PhotoTitleRoute route a title is specified
+            //Arrange
+            var context = new FakeHttpContextForRouting(requestUrl: "~/Photo/Title/My%20Photo");
+            var routes = new RouteCollection();
+            PhotoSharingApplication.RouteConfig.RegisterRoutes(routes);
+
+            // Act
+            RouteData routeData = routes.GetRouteData(context);
+
+            // Assert
+            Assert.IsNotNull(routeData);
+            Assert.AreEqual("Photo", routeData.Values["controller"]);
+            Assert.AreEqual("DisplayByTitle", routeData.Values["action"]);
+            Assert.AreEqual("My%20Photo", routeData.Values["title"]);
+        }
+
+    }
+}
