7080d40ccc6c2290d212d20f821494f6118769bd
Jens Lindhardt <jens@lindhardt-freelance.dk>
2017-05-03 22:51:00 +0200

    Demo starter created

---
+++ module 08 - Applying Styles to ASP.NET MVC/Demo/OperasWebSite/OperasWebSite/Models/OperasInitializer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace OperasWebSite.Models
+{
+    public class OperasInitializer : DropCreateDatabaseAlways<OperasDB>
+    {
+        protected override void Seed(OperasDB context)
+        {
+            base.Seed(context);
+
+            var operas = new List<Opera>
+            {
+                new Opera {
+                    Title = "Cosi Fan Tutte",
+                    Year = 1790,
+                    Composer = "Wolfgang Amadeus Mozart",
+                },
+                new Opera {
+                    Title = "Rigoletto",
+                    Year = 1851,
+                    Composer = "Giuseppe Verdi",
+                },
+                new Opera {
+                    Title = "Nixon in China",
+                    Year = 1987,
+                    Composer = "John Adams"
+                },
+                new Opera {
+                    Title = "Wozzeck",
+                    Year = 1922,
+                    Composer = "Alban Berg"
+                }
+            };
+
+            operas.ForEach(s => context.Operas.Add(s));
+            context.SaveChanges();
+        }
+    }
+}
