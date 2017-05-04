b685f04a74e323bacd36407aacada235a8d18d2b
Jens Lindhardt <jens@lindhardt-freelance.dk>
2017-05-04 19:15:27 +0200

    Module 09 Lab Starter

---
+++ module 09 - Building Responsive Pages in ASP.NET MVC/Lab/Comment Components/CommentController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PhotoSharingApplication.Models;
+
+namespace PhotoSharingApplication.Controllers
+{
+    public class CommentController : Controller
+    {
+        private IPhotoSharingContext context;
+
+        //Constructors
+        public CommentController()
+        {
+            context = new PhotoSharingContext();
+        }
+
+        public CommentController(IPhotoSharingContext Context)
+        {
+            context = Context;
+        }
+
+        //
+        // GET: /Comment/Delete/5
+        public ActionResult Delete(int id = 0)
+        {
+            Comment comment = context.FindCommentById(id);
+            ViewBag.PhotoID = comment.PhotoID;
+            if (comment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(comment);
+        }
+
+        //
+        // POST: /Comment/Delete/5
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Comment comment = context.FindCommentById(id);
+            context.Delete<Comment>(comment);
+            context.SaveChanges();
+            return RedirectToAction("Display", "Photo", new { id = comment.PhotoID });
+        }
+
+    }
+}
