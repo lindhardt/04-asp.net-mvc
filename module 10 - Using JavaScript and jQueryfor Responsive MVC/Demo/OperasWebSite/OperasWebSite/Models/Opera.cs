2dfb5508271861e2e5763c5e36af27e61241c1b3
Jens Lindhardt <jens@lindhardt-freelance.dk>
2017-05-07 20:50:08 +0200

    Module 10 demo prep

---
+++ module 10 - Using JavaScript and jQueryfor Responsive MVC/Demo/OperasWebSite/OperasWebSite/Models/Opera.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OperasWebSite.Models
+{
+    public class Opera
+    {
+        public int OperaID { get; set; }
+        [Required]
+        [StringLength(200)]
+        public string Title { get; set; }
+        [CheckValidYear]
+        public int Year { get; set; }
+        [Required]
+        public string Composer { get; set; }
+
+        public virtual ICollection<Review> Reviews { get; set; }
+
+    }
+
+    public class CheckValidYear : ValidationAttribute
+    {
+        public CheckValidYear()
+        {
+            ErrorMessage = "The earliest opera is Daphne, 1598, by Corsi, Peri, and Rinuccini";
+        }
+
+        public override bool IsValid(object value)
+        {
+            int year = (int)value;
+            if (year < 1598)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ module 10 - Using JavaScript and jQueryfor Responsive MVC/Demo/OperasWebSite/OperasWebSiteTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OperasWebSiteTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OperasWebSiteTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("07ede72b-3ca7-40b1-b07e-9d63b5958701")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
