0835405b7e0171f68cc4e7bc4b80f880855fdd85
Jens Lindhardt <jens@lindhardt-freelance.dk>
2017-05-07 20:05:51 +0200

    Module 10 - Starter prep from module 9

---
+++ module 10 - Using JavaScript and jQueryfor Responsive MVC/Lab/Starter/PhotoSharingApplication/PhotoSharingApplication/Models/PhotoSharingContext.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PhotoSharingApplication.Models
+{
+    public class PhotoSharingContext : DbContext, IPhotoSharingContext
+    {
+        public DbSet<Photo> Photos { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
+        IQueryable<Photo> IPhotoSharingContext.Photos
+        {
+            get { return Photos; }
+        }
+
+        IQueryable<Comment> IPhotoSharingContext.Comments
+        {
+            get { return Comments; }
+        }
+
+        int IPhotoSharingContext.SaveChanges()
+        {
+            return SaveChanges();
+        }
+
+        T IPhotoSharingContext.Add<T>(T entity)
+        {
+            return Set<T>().Add(entity);
+        }
+
+        Photo IPhotoSharingContext.FindPhotoById(int ID)
+        {
+            return Set<Photo>().Find(ID);
+        }
+
+        Photo IPhotoSharingContext.FindPhotoByTitle(string Title)
+        {
+            Photo photo = (from p in Set<Photo>()
+                           where p.Title == Title
+                           select p).FirstOrDefault();
+            return photo;
+        }
+
+        Comment IPhotoSharingContext.FindCommentById(int ID)
+        {
+            return Set<Comment>().Find(ID);
+        }
+
+
+        T IPhotoSharingContext.Delete<T>(T entity)
+        {
+            return Set<T>().Remove(entity);
+        }
+    }
+}
---
+++ module 10 - Using JavaScript and jQueryfor Responsive MVC/Lab/Starter/PhotoSharingApplication/PhotoSharingTest/Doubles/FakeHttpClasses.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+
+namespace PhotoSharingTest.Doubles
+{
+
+    public class FakeHttpContextForRouting : HttpContextBase
+    {
+        FakeHttpRequestForRouting _request;
+        FakeHttpResponseForRouting _response;
+
+        public FakeHttpContextForRouting(string appPath = "/", string requestUrl = "~/")
+        {
+            _request = new FakeHttpRequestForRouting(appPath, requestUrl);
+            _response = new FakeHttpResponseForRouting();
+        }
+
+        public override HttpRequestBase Request
+        {
+            get { return _request; }
+        }
+
+        public override HttpResponseBase Response
+        {
+            get { return _response; }
+        }
+    }
+
+    public class FakeHttpRequestForRouting : HttpRequestBase
+    {
+        string _appPath;
+        string _requestUrl;
+
+        public FakeHttpRequestForRouting(string appPath, string requestUrl)
+        {
+            _appPath = appPath;
+            _requestUrl = requestUrl;
+        }
+
+        public override string ApplicationPath
+        {
+            get { return _appPath; }
+        }
+
+        public override string AppRelativeCurrentExecutionFilePath
+        {
+            get { return _requestUrl; }
+        }
+
+        public override string PathInfo
+        {
+            get { return ""; }
+        }
+
+        public override NameValueCollection ServerVariables
+        {
+            get { return new NameValueCollection(); }
+        }
+    }
+
+    public class FakeHttpResponseForRouting : HttpResponseBase
+    {
+        public override string ApplyAppPathModifier(string virtualPath)
+        {
+            return virtualPath;
+        }
+    }
+
+
+}
