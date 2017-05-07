ef12e8431cc0b999bafb404fce3dddadd99af4a6
Jens Lindhardt <jens@lindhardt-freelance.dk>
2017-05-07 20:15:10 +0200

    Module 10 - Stater updated

--- module 10 - Using JavaScript and jQueryfor Responsive MVC/Lab/Starter/PhotoSharingApplication/PhotoSharingApplication/Controllers/PhotoController.cs
+++ module 10 - Using JavaScript and jQueryfor Responsive MVC/Lab/Starter/PhotoSharingApplication/PhotoSharingApplication/Controllers/PhotoController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI;
 using System.Globalization;
 using PhotoSharingApplication.Models;
 
