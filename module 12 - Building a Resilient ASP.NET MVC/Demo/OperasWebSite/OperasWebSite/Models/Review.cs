122d41081732587b15f1ec476235e394cf6b261c
Jens Lindhardt <jens@lindhardt-freelance.dk>
2017-05-10 09:34:12 +0200

    Demo prep added

---
+++ module 12 - Building a Resilient ASP.NET MVC/Demo/OperasWebSite/OperasWebSite/Models/Review.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OperasWebSite.Models
+{
+    public class Review
+    {
+        public int ReviewID { get; set; }
+
+        public int OperaID { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [DisplayName("Performance Date")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yy}", ApplyFormatInEditMode = true)]
+        public DateTime Date { get; set; }
+        
+        [Required]
+        public string Company { get; set; }
+
+        [Required]
+        [DisplayName("Review")]
+        public string ReviewText { get; set; }
+
+        public virtual Opera Opera { get; set; }
+
+    }
+}
