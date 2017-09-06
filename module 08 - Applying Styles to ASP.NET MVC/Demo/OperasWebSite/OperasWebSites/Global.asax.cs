42c0cd9e55012be07d9ea2f0d7891c3160a53018
Jens Lindhardt <jens@lindhardt-freelance.dk>
2017-09-06 16:42:03 +0200

    Added Demo to module 8

---
+++ module 08 - Applying Styles to ASP.NET MVC/Demo/OperasWebSite/OperasWebSites/Global.asax.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using OperasWebSite.Models;
+
+namespace OperasWebSite
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            Database.SetInitializer(new OperasInitializer()); 
+
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        
+    }
+}
---
+++ module 08 - Applying Styles to ASP.NET MVC/Demo/OperasWebSite/OperasWebSites/Models/Opera.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.DynamicData;
+
+namespace OperasWebSite.Models
+{
+    public class Opera
+    {
+        public int OperaID { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Title { get; set; }
+
+        [Required]
+        [CheckValidYear]
+        public int Year { get; set; }
+
+        public string Composer { get; set; }
+    }
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public class CheckValidYear : ValidationAttribute
+    {
+        public CheckValidYear()
+        {
+            ErrorMessage = "The earliest opera is Daphe. 1598, Peri and Rinuccini";
+        }
+
+        public override bool IsValid(object value)
+        {
+            try
+            {
+                int year = (int)value;
+                return year >= 1598;
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException("Exception casting value to int", e);
+            }
+        }
+    }
+}
